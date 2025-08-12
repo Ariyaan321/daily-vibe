4a15cdb18dc2a05523fbaf3efafba7a1b55d5a88
Ariyaan <134138177+Ariyaan321@users.noreply.github.com>
2025-08-12 15:17:56 +0530

    initial commit

---
+++ src/main/java/com/dailyvibeproj/daily_vibe/DailyVibeApplication.java
@@ -0,0 +1,13 @@
+package com.dailyvibeproj.daily_vibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyVibeApplication.class, args);
+	}	
+
+}
---
+++ src/test/java/com/dailyvibeproj/daily_vibe/DailyVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyvibeproj.daily_vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
